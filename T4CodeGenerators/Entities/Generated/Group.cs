1d902cc14db099fafe643446c48512f3ee27d18d
Tim Maes <91606949+Tim-Maes@users.noreply.github.com>
2023-10-05 14:28:19 +0200

    Add project files.

---
+++ T4CodeGenerators/Entities/Generated/Group.cs
@@ -0,0 +1,14 @@
+// This file is auto generated. Changes to this file will be lost!
+using System;
+using System.Collections.Generic;
+	
+namespace T4CodeGenerators.Entities.Generated;
+
+public class Group
+{
+		
+	public string Name { get; set; }
+		
+	public List<User> Members { get; set; }
+}
+
---
+++ T4CodeGenerators/Entities/Generated/User.cs
@@ -0,0 +1,25 @@
+﻿// This file is auto generated. Changes to this file will be lost!
+using System;
+using System.Collections.Generic;
+	
+namespace T4CodeGenerators.Entities.Generated;
+
+/// <summary>
+/// This is a user
+/// </summary>
+public class User
+{
+		
+	public string FirstName { get; set; }
+		
+	public string LastName { get; set; }
+		
+	public string EmailAddress { get; set; }
+		
+	public DateTimeOffset DateOfBirth { get; set; }
+		
+	public Group Group { get; set; }
+		
+	public int? NullableNumber { get; set; }
+}
+
